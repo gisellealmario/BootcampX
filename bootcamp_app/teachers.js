255f3126644076083c291a7fc3aefd42c55af0af
gisellealmario <gisellealmario@gmail.com>
2023-11-27 11:09:52 +0000

    sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,18 +6,22 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+ORDER BY teacher
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+  console.log(res);
+})
+.catch(err => console.error('query error', err.stack));
